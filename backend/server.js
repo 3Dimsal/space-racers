1c8a3859be5e700f2e854c345f2c9fd61078d4c6
3Dimsal <86879553+3Dimsal@users.noreply.github.com>
2023-11-06 04:05:52 -0500

    all

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from "path";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 import postRoutes from './routes/posts.js';
 
@@ -22,15 +23,35 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
+
+// const client = new MongoClient(process.env.CONNECTION_URL, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+  
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+// }
+
+
+// run().catch(console.dir);
 
 
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = `https://space-cruisers.herokuapp.com/posts`;
+const url = `https://space-racing.herokuapp.com/posts`;
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
